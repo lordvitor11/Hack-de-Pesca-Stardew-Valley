8d3da668bd8feba4b344f6e40a8d1c190b9c4f78
Vitor César <65216891+lordvitor11@users.noreply.github.com>
2022-08-31 02:56:06 -0300

    Update main.py
    -Mudança na biblioteca de interface gráfica usada(PySimpleGUI => Tkinter);
    
    -Modo estrutural de programação mudada para orientada a objetos.
    
    Funcionamento atual:
    É necessário clicar no botão "Começar"  para que o hack consiga reconhecer as teclas pressionadas. De início, o comando para jogar a vara de pesca é CTRL + J, fazendo que sempre acerte o ponto "Max" da vara, e também é necessário apertar "Esc" duas vezes antes de fechar o hack para que não trave.

--- main.py
+++ main.py
@@ -1,78 +1,45 @@
-import keyboard;
-# from pynput.keyboard import Key, Listener;
-# import time;
-import PySimpleGUI as sg;
+from tkinter import *;
+from pynput.keyboard import Key, Listener;
 import pyautogui as pg;
+import time;
 
-# def setLanguage(lang):
-#     global language, layout2;
-#     language = langList[lang];
+class App:
+    def __init__(self, master=None):
+        self.keys = [];
+        self.btn = Button(master);
 
-#     if (language == "pt-br"):
-#         layout2 = [[sg.Text("Escolha a vara de pesca que você está utilizando:")],
-#                    [sg.Combo(["Vara de treinamento", "Vara de bambu", "Vara de fibra de vidro", "Vara de irídio"], default_value="Vara de bambu")],
-#                    [sg.Button("Sair")]];
-#     elif (language == "pt"):
-#         layout2 = [[sg.Text("Escolha a cana de pesca que está a usar:")],
-#                    [sg.Combo(["Vara de treinamento", "Vara de bambu", "Vara de fibra de vidro", "Vara de irídio"], default_value="Vara de bambu")],
-#                    [sg.Button("Sai")]];
-#     elif (language == "en"):
-#         layout2 = [[sg.Text("Choose the fishing rod you are using:")],
-#                    [sg.Combo(["Training rod", "Bamboo pole", "Fiberglass rod", "Iridium rod"], default_value="Bamboo Pole")],
-#                    [sg.Button("Exit")]];
+        self.btn["text"] = "Começar";
+        self.btn.bind("<Button-1>", self.start);
 
-# language = "";
+        self.btn.pack();
 
-# langList = {"Português do Brasil":"pt-br", "Português de Portugal":"pt", "English":"en"};
+    def start(self, event):
+        with Listener(on_press = self.show) as listener:
+            listener.join();
 
-# Language layout
-# layout1 = [[sg.Text("Select your language:")],
-#            [sg.Combo(["Português do Brasil", "Português de Portugal", "English"], default_value="English", key="comboLang")],
-#            [sg.Button("Confirm")]];
+    def show(self, key):
+        self.makeAction(key);
 
-# layout2 = [];
+    def makeAction(self, key):
+        if (len(self.keys) == 2):
+            del self.keys[0];
+        if (len(self.keys) < 3):
+            self.keys.append(key);
+        
+        try:
+            if (self.keys[0] == Key.ctrl_l and self.keys[1].char == "j"):
+                self.click();
+        except:
+            pass
 
-# # Layout manager
-# layout = [[sg.Column(layout1, key="COL1")],
-#           [sg.Column(layout2, key="COL2", visible=False)]];
+        if (len(self.keys) > 1):
+            if (self.keys[0] == Key.esc and self.keys[1] == Key.esc):
+                self.keys.clear();
+                exit();
 
-keys = []
-def show(key):
-    global keys;
-    keys.append(key);
-    print(keys)
-    if (len(keys) == 3):
-        del keys[0];
-    else:
-        if (keys[0] == Key.ctrl and keys[1] == "j"):
-            pg.mouseDown(button=left, duration=2);
-            # time.sleep(2);
-            # pg.mouseUp();
+    def click(self):
+        pg.mouseDown(button="left"); time.sleep(0.9); pg.mouseUp();
 
-layout = [[sg.Text("Choose the fishing rod you are using:")],
-          #[sg.Combo(["Training rod", "Bamboo pole", "Fiberglass rod", "Iridium rod"], default_value="Bamboo Pole")],
-          [sg.Button("Exit")]];
-
-mainWindow = sg.Window("Hack Stardew Valley", layout);
-
-# layout = 1;
-
-while (True):
-    event, values = mainWindow.read();
-    if (event == sg.WIN_CLOSED or event == "Sair"):
-        break;
-
-    while (True):
-        if (keyboard.is_pressed("q")):
-            print("a")
-
-    # with Listener(on_press = show) as listener:
-    #     listener.join();
-    
-    # if (event == "Confirm"):
-    #     setLanguage(values["comboLang"]);
-    #     mainWindow[f"COL{layout}"].update(visible=False);
-    #     layout += 1;
-    #     mainWindow[f"COL{layout}"].update(visible=True);
-
-mainWindow.close();
+root = Tk();
+App(root);
+root.mainloop();
